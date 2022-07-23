8c000938594749c2c14a8bc8132d367f1c60a114
Oliver Phan <oliver.jlphan@gmail.com>
2022-07-23 17:52:00 -0400

    Add Angular router

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: NxWelcomeComponent },
+  { path: 'todo-list', component: TodoListComponent, data: { title: 'List of todos' } },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, TodoListComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false }
+    )
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
